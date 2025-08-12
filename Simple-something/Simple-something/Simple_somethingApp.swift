7e76923530d1eedb5fc739912f4d8cf28563a509
Jaro <banan_08depeszowac@icloud.com>
2025-08-12 17:04:35 +0200

    updated gitignore and created new empty Xcode project

---
+++ Simple-something/Simple-something/Simple_somethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_somethingApp.swift
+//  Simple-something
+//
+//  Created by Jaromir Jagieluk on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Simple_somethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple-something/Simple-somethingTests/Simple_somethingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_somethingTests.swift
+//  Simple-somethingTests
+//
+//  Created by Jaromir Jagieluk on 12.08.2025.
+//
+
+import Testing
+@testable import Simple_something
+
+struct Simple_somethingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
